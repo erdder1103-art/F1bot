4bc4ebc2df8c7e891ef924eeac4ac027de75f5f8
erdder1103-art <erdder1103@gmail.com>
2026-02-13 21:30:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const URL_GROUP = "https://t.me/livebigbrother";
 const URL_SUPPORT = "https://t.me/F1top_bro";
 
 function nowStr() {
-  return dayjs().format("YYYY/MM/DD HH:mm");
+  return dayjs().utcOffset(8).format("YYYY/MM/DD HH:mm");
 }
 
 // ====== 寫入佇列 ======
@@ -151,3 +151,4 @@ console.log("Bot is running...");
     console.error("GAS TEST failed:", err);
   }
 })();
+
